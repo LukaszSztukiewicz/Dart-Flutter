133288b4608b9eaf9e1ee490a53e79e02673adf2
lukasz sz <sztukiewicz.lukasz@gmail.com>
2020-05-21 13:12:19 +0200

    6

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,50 +1,147 @@
 import 'package:flutter/material.dart';
-import 'package:flare_flutter/flare_actor.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-  ),  
-);
+void main(List<String> args) => runApp(MaterialApp(
+  home: MyApp(),
+));
 
-class Home extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("Helllo World My Guys"),
-      centerTitle: true,
-      backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      appBar: AppBar(
+        title: Text("Whales meeting"),
+        backgroundColor: Colors.black54,
+        centerTitle: true,
       ),
-    body: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: <Widget>[
-        SizedBox(
-          width: 100,
-          height: 100,
-          child: Container(
-            color: Colors.blueGrey,
-          ),
+      body: Container(
+        color: Colors.black87,
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Icon(
+                    Icons.wb_iridescent,
+                    color: Colors.cyan,
+                    size: 50,
+                  ),
+                )
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ClipOval(child:Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.grey,
+                  child: FittedBox(
+                    fit: BoxFit.fill,
+                    child: Image.asset("assets/leaves.jpg")
+                    )
+                ),)
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),
+                  Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),Icon(Icons.wb_iridescent,color: Colors.cyan),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(
+                    Icons.label_important,
+                    size: 50,
+                    color: Colors.cyan
+                  ),
+                ),
+                Container(
+                 child: Text(
+                   "Level:", 
+                   textAlign: TextAlign.center, 
+                   style: TextStyle(
+                     fontFamily: "LobsterTwo",
+                     fontSize: 30,
+                     color: Colors.white,
+                   ), ),
+                  ),
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "42",
+                        style: TextStyle(
+                          color: Colors.white,
+                           fontSize: 25,
+                           fontFamily: "LobsterTwo",
+                           )
+                      ),
+                    ),
+                  )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(
+                    Icons.email,
+                    size: 40,
+                    color: Colors.cyan
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    "turbolukas3002@gmail.com", 
+                    textAlign: TextAlign.center, 
+                    style: TextStyle(
+                      fontFamily: "LobsterTwo",
+                      fontSize: 30,
+                      color: Colors.white,
+                    ), ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Icon(
+                    Icons.phone_android,
+                    size: 40,
+                    color: Colors.cyan
+                  ),
+                ),
+                Container(
+                 child: Text(
+                   "608398229", 
+                   textAlign: TextAlign.center, 
+                   style: TextStyle(
+                     fontFamily: "LobsterTwo",
+                     fontSize: 30,
+                     color: Colors.white,
+                   ), ),
+                  )
+              ],
+            ),
+
+
+          ],
         ),
-        Expanded(child: Container(
-          color: Colors.red,
-        ),
-        ),
-      ]),
-    floatingActionButton: FloatingActionButton(
-      onPressed: (){
-        print("clicked");
-      },
-      backgroundColor: Colors.amberAccent,
-      child: Text(
-        "click",
-        style: TextStyle(
-          fontFamily: 'LobsterTwo',
-          fontStyle: FontStyle.italic,
-          color: Colors.black,
-        ),
-        ),
-    ),
+      ),
     );
   }
 }
---
+++ myapp/lib/main_bkp2.dart
@@ -0,0 +1,41 @@
+/*import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+  ),  
+);
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("Helllo World My Guys"),
+      centerTitle: true,
+      backgroundColor: Color.fromARGB(255, 255, 0, 0),
+      ),
+    body: Container(
+      child: Icon(
+        Icons.view_compact,
+        color: Colors.teal,
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){
+        print("clicked");
+      },
+      backgroundColor: Colors.amberAccent,
+      child: Text(
+        "click",
+        style: TextStyle(
+          fontFamily: 'LobsterTwo',
+          fontStyle: FontStyle.italic,
+          color: Colors.black,
+        ),
+        ),
+    ),
+    );
+  }
+}
+*/
