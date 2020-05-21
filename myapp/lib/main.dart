8bddb0c29ac86d22d241449ced68628dee460e62
lukasz sz <sztukiewicz.lukasz@gmail.com>
2020-05-21 22:05:30 +0200

    9

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
+Color a = Colors.white;
+Color b = Colors.black54;
 void main(List<String> args) => runApp(MaterialApp(
   title: "Lukas App",
   home: MyApp(),
@@ -227,16 +230,105 @@ class SecondPage extends StatelessWidget {
              )
            ),
            Padding(
-             padding: const EdgeInsets.all(30.0),
-             child: FloatingActionButton(
-               onPressed: (){Navigator.pop(context);},
-               backgroundColor: Colors.cyan,
-               child: Icon(Icons.offline_bolt, color: Colors.white,)
-               ,),
-           )
+             padding: const EdgeInsets.all(20.0),
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.center,
+               children: <Widget>[
+                 RaisedButton.icon(onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThirdPAge()),
+                        );
+                 }, icon: Icon(Icons.gps_not_fixed, color: Colors.white,), label: Text("Third", style: TextStyle( fontFamily: "LobsterTwo", color: Colors.white),), color: Colors.pink,)
+               ],
+             ),
+           ),
+           /*Container(
+             child: Padding(
+               padding: const EdgeInsets.all(30.0),
+               child: FloatingActionButton(
+                 onPressed: (){Navigator.pop(context);},
+                 backgroundColor: Colors.cyan,
+                 child: Icon(Icons.offline_bolt, color: Colors.white,)
+                 ),
+             ),
+           ),*/
          ],
        ), 
       ),
     );
   }
 }
+
+class ThirdPAge extends StatefulWidget {
+  @override
+  _ThirdPAgeState createState() => _ThirdPAgeState();
+}
+
+class _ThirdPAgeState extends State<ThirdPAge> {
+
+  List<Quote> quotes = [
+    Quote('Value your time ro someone else will value it for you', 'Unknown'),
+    Quote('If you want to keep your employers coming to work, keep them poor', 'Rich Dad'),
+    Quote('Future starts today, not tomorrow', 'John Paul II')
+  ];
+
+  Widget buildCard(Quote){
+    return Card(
+      color: Colors.cyan,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+          Text(
+            '"'+Quote.text+'"',
+            style: TextStyle(
+              fontFamily: "LobsterTwo",
+              color: a,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+           height: 5, 
+          ),
+            Text(
+            " - "+Quote.author,
+            style: TextStyle(
+              fontFamily: "LobsterTwo",
+              color: a,
+              fontSize: 20,
+            ),
+          ),
+        
+          ]
+        ),
+      )
+    );
+    }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Text("ThirdPage"),
+      ),
+      backgroundColor: b,
+      body: Container(
+        color: b,
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: quotes.map((q) => buildCard(q)).toList() 
+          ),
+        ),
+
+      ),
+    );
+  }
+}
+
