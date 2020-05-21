6e00f859e009e677f9dfa0187b5dbaf02cfc69c9
lukasz sz <sztukiewicz.lukasz@gmail.com>
2020-05-21 10:07:22 +0200

    5

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 void main() => runApp(MaterialApp(
   home: Home(),
@@ -14,19 +15,33 @@ class Home extends StatelessWidget {
       centerTitle: true,
       backgroundColor: Color.fromARGB(255, 255, 0, 0),
       ),
-    body: Center(
-        
-    ),
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(
+          width: 100,
+          height: 100,
+          child: Container(
+            color: Colors.blueGrey,
+          ),
+        ),
+        Expanded(child: Container(
+          color: Colors.red,
+        ),
+        ),
+      ]),
     floatingActionButton: FloatingActionButton(
       onPressed: (){
         print("clicked");
       },
-      backgroundColor: Color.fromARGB(255, 255,2, 20),
+      backgroundColor: Colors.amberAccent,
       child: Text(
         "click",
         style: TextStyle(
           fontFamily: 'LobsterTwo',
           fontStyle: FontStyle.italic,
+          color: Colors.black,
         ),
         ),
     ),
